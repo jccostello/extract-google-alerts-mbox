814a8cf33a2187d43cc13d3e11175825b91f8433
John Costello <36679492+jccostello@users.noreply.github.com>
2024-01-26 08:27:44 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-# extract-google-alerts-mbox
+
+# extract_google_alerts_mbox.py
+
+## Overview
+
+This script extracts individual articles from each Google Alert email and
+export the results to a tab-delimited file specified by you.
+
+## Usage
+
+python extract_google_alerts_mbox.py -m MBOX_ARCHIVE -o FILENAME
+
+### Input file format
+
+This script takes as input an archive file in mbox format containing Google
+Alerts. I used [Google Takeout](https://takeout.google.com/) to export the alerts
+from Gmail.
+
+### About Google Alert emails
+
+Google Alert emails contain summaries of and links to articles about a chosen
+topic. The article entries provide potentially useful data, such as the URL
+of the article reported by the Alert, as well as the article's title,
+publisher, and teaser text.
+
+### Output file format
+
+The tab-delimited output contains the following columns:
+
+* Message date
+* Unique message-id
+* Article URL
+* Article title
+* Article publisher
+* Article teaser, often a summary of the article
+
+## Background
+
+I wrote this script to process four years of Google Alerts targeting a
+specific topic. For this script to be useful to you, you can set up a
+Google Alert for a topic of interest and let the alerts accumulate long
+enough to create a substantial data set.
+
+One possible use of the data is to tokenize words found in the title and
+teaser fields, then map how word usage and phrase usage changes over time.
+
+## Technical notes
+
+### Data validation
+
+The script performs minimal data validation, verifying only that the mbox
+messages are not defective due to incomplete headers (as defined by
+mailbox.mbox.defects) and that the date field is valid.
+
+The script uses extruct.MicrodataExtractor to extract structured content
+that has itemtype `http://schema.org/Article`.
+
+### Cleaning bold tags using BeautifulSoup
+
+**Short version:**
+
+extruct.MicrodataExtractor replaces style tags with Unicode whitespace.
+
+If you have text such as `<b>AI</b>-powered`, the cleaned
+version will have an extra space: 'AI -powered'. That extra space adds
+work to tokenizing hyphenated words.
+
+**Longer version:**
+
+extruct.MicrodataExtractor (using the underlying lxml.html.clean library)
+replaces style tags with whitespace. There does not appear to be a way to
+override this functionality.
+
+Some of the content I extracted included style tags within hyphenated words.
+When extruct.MicrodataExtractor cleans `<b>AI</b>-powered`, an extra space is
+inserted before the hyphen, resulting in 'AI -powered'.
+
+Since I want more control over how I tokenize hyphenated words, I added a step
+before extraction to remove all bold tags. This did not significantly
+increase the processing time for four years of alerts containing 10,000+
+articles.
---
+++ extract_google_alerts_mbox.py
@@ -0,0 +1,140 @@
+# This script extracts content from an archive file that uses mbox format
+# and loads selected fields for eventual output to a CSV.
+# Each mbox message contains multiple articles.
+# Each row output will include email message date, email message id,
+# and fields extracted from a specific article in the email message.
+
+import sys
+if not sys.version_info > (2, 7):
+    sys.exit("You need to use a version of Python 3 earlier than Python 3.12.")
+elif not sys.version_info < (3, 12):
+    sys.exit("Use Python 3.11 or earlier to avoid an issue with 3.12 and extruct.")
+
+import os
+import mailbox
+import csv
+import urllib.parse
+from bs4 import BeautifulSoup
+from email.utils import parsedate_tz
+from extruct import MicrodataExtractor
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Extract articles from a mailbox of Google Alerts to CSV.",
+    epilog="The mbox file should be created using Google Takeout.")
+parser.add_argument('-m', '--mbox', dest='mbox_file', 
+                    required=True, help='Mailbox file (mbox format) to process')
+parser.add_argument('-o', '--output', dest='output_file', required=True,
+                    help='Where to output the extracts')
+args = parser.parse_args()
+
+# Variables
+messages_parsed = []
+messages_total = 0
+messages_defective = 0
+article_list = []
+
+# Before trying to open it with mailbox.mbox(), verify that the mbox file 
+# exists. If the file doesn't exist, mailbox.mbox() will try to create
+# an empty file using the filename provided.
+if not os.path.isfile(args.mbox_file):
+    sys.exit("The mailbox file doesn't exist at the given location:", args.mbox_file)
+if not os.access(args.mbox_file, os.R_OK):
+    sys.exit("The mailbox file cannot be read:", args.mbox_file)
+
+archive = mailbox.mbox(args.mbox_file)
+
+# For each message, get headers (date, message id) and HTML content.
+# Skip messages if there is a defect with some aspect of the message.
+for key in archive.iterkeys():
+    message = archive[key]
+    messages_total = messages_total + 1
+
+    if message.defects:
+        print("Skipping message", key, "because of the following defects:", message.defects)
+        messages_defective = messages_defective + 1
+        continue    
+
+    for step in message.walk():
+        if 'html' in step.get_content_type():
+            # Decode the payload twice, once from base64 by get_payload() and
+            # again from the binary object that get_payload() returns.
+            # Pass decode=True to get_payload() to decode the base64 content.
+            # Use .decode() to decode this binary object as a string.
+            html_content = step.get_payload(decode=True).decode('utf-8')
+
+            # Parse the mailbox-friendly date into an 8-digit date value.
+            date_tuple = parsedate_tz(message['date'])
+            if date_tuple:
+                date_mdcy = ''.join([str("{:0>2}").format(date_tuple[1]), 
+                                    str("{:0>2}").format(date_tuple[2]), 
+                                    str("{:0>2}").format(date_tuple[0])])
+            else:
+                print("Skipping message", key, "because the date couldn't be parsed:", message['date'])
+                messages_defective = messages_defective + 1
+                continue
+
+            message_id = message['message-ID']
+            
+            # The message date and message ID will be added to the 
+            # final output, while the html content will be parsed further
+            # into individual articles for output.
+            messages_parsed.append([date_mdcy, message_id, html_content])
+
+print("Total messages in archive:", messages_total)
+print("Total defective messages:", messages_defective)
+print("Total messages with content successfully extracted:", len(messages_parsed))
+
+try:
+    archive.close
+    print("Mailbox archive closed.")
+except IOError:
+    print("There was an error closing the mailbox archive.")
+
+# From each entry in messages_parsed:
+# - parse the entry's html content
+# - find all articles within the html
+# - clean bold tags before extruct.MicrodataExtractor does
+# - extract each article's title, teaser, publisher, and url.
+for entry in messages_parsed:
+    soup = BeautifulSoup(entry[2], "html.parser")
+    articles = soup.findAll("tr", {"itemtype": 'http://schema.org/Article'})
+    for article in articles:
+        # Before extracting data via MicrodataExtractor, remove <b> and </b>
+        # tags to avoid breaking hyphenated words with extra spaces (and
+        # avoid creating future problems for word tokenizing).
+        # For example, MicrodataExtractor (using lxml.html.clean) transforms 
+        #'<b>AI</b>-powered' into 'AI -powered' rather than 'AI-powered'.
+        for bold_tag in article.find_all('b'):
+            bold_tag.replace_with_children()
+        article_text = str(article)
+
+        # Extract article data into a dict.
+        article_data = MicrodataExtractor().extract(article_text)
+        
+        # Trim Google's redirect code from the article URL.
+        google_url = article_data[0]['properties']['url']
+        parsed_url=urllib.parse.urlparse(google_url)
+        url = urllib.parse.parse_qs(parsed_url.query).get('url', [''])[0]
+        
+        title = article_data[0]['properties']['name']
+        teaser = article_data[0]['properties']['description']
+        publisher = article_data[0]['properties']['publisher']['properties']['name']
+
+        # The first field is the date from the parent email message.
+        # The second field is the unique message id.
+        article_list.append([entry[0], entry[1], url, title, publisher, teaser])
+
+print("Extracted", len(article_list), 
+      "Google Alert articles from the mailbox archive.")
+
+# Export the article extracts to CSV
+try:
+    with open(args.output_file, 'w', newline='') as article_file:
+        article_writer = csv.writer(article_file, delimiter='\t')
+        article_writer.writerows(article_list)
+        print("All Google Alert articles output.")
+except IOError:
+    print("Error while outputting articles to CSV file.")
+
+exit()
